186fd2c759c277288e207e9bea67bd16c62195d2
kun368 <kun368@126.com>
2021-10-10 22:44:22 +0800

    update

---
+++ handson-ml2/mnist/main.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_predict, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def show_image(bits):
+    bits = np.reshape(bits, (28, 28))
+    plt.imshow(bits, cmap='binary')
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == '__main__':
+    mnist = fetch_openml('mnist_784', version=1, as_frame=True)
+    print(mnist.keys())
+
+    X, y = pd.DataFrame(mnist['data']), pd.Series(mnist['target']).astype(dtype=np.uint8)
+    X.info()
+    print(X.shape, y.shape)
+
+    show_image(X.iloc[0].values)
+    X_train, X_test = X[:60000], X[60000:]
+    y_train, y_test = y[:60000], y[60000:]
+
+    model = KNeighborsClassifier()
+
+    train_res = cross_val_predict(model, X_train, y_train, cv=3)
+    print(list(y_train)[:10])
+    print(list(train_res)[:10])
+    print(classification_report(y_train, train_res, digits=3))
+
+    model.fit(X_train, y_train)
+    test_res = model.predict(X_test)
+    print(list(y_test)[:10])
+    print(list(test_res)[:10])
+    print(classification_report(y_test, test_res, digits=3))
